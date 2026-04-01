35efd535ac6ad73cde6872e635ec30e0c437d603
RomeoSuiii <zockendebanane@gmx.de>
2026-04-01 17:00:13 +0200

    Add cards object with medieval, jobs, and pirates categories

---
+++ cards.js
@@ -0,0 +1,19 @@
+const cards = {
+  medieval: ["Golden Apple","Royal Banana","Knight Lemon","Castle Orange","Dragon Fruit","Crown Pear","Medieval Cherry","Shield Grape","Sword Mango","King Kiwi"],
+
+  jobs: [
+    "Office Assistant","Cashier","Delivery Driver","Barista","Janitor",
+    "Retail Worker","Receptionist","Warehouse Worker","Call Center Agent","Security Guard",
+    "Software Developer","Nurse","Teacher","Electrician","Mechanic",
+    "Graphic Designer","Pilot","Chef","Firefighter","Police Officer",
+    "Surgeon","Architect","Lawyer","CEO","Scientist"
+  ],
+
+  pirates: [
+    "Deckhand","Sailor","Cook","Cabin Boy","Lookout",
+    "Map Carrier","Rope Handler","Gunner Helper","Supply Carrier","Ship Cleaner",
+    "Pirate Captain","First Mate","Navigator","Cannoneer","Quartermaster",
+    "Boatswain","Swordsman","Navigator Specialist","Treasure Hunter","Ship Gunner",
+    "Legendary Pirate Captain","Kraken Tamer","Ghost Pirate","Sea War Strategist","Master of the Seven Seas"
+  ]
+};
